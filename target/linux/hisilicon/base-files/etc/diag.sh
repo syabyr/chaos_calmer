db630570c7a38f9e0db4c054b0c36a2281084921
Igor Zalatov <flyrouter@gmail.com>
2019-02-28 00:17:03 +0300

    Add basic GPIO init

--- target/linux/hisilicon/base-files/etc/diag.sh
+++ target/linux/hisilicon/base-files/etc/diag.sh
@@ -6,6 +6,21 @@
 . /lib/functions/leds.sh
 . /lib/hisilicon.sh
 
+
+gpio_init() {
+	case $(hisilicon_board_name) in
+	hi3518ev100)
+		echo "No GPIO settings" | logger
+		;;
+	hi3518ev200)
+		devmem 0x200f0060 32 0x0 && echo " | GPIO4_3 | GPIO35 | alarm_out | Board XM 00018520                                | SoC hi3518ev200 | " | logger -t gpio_init
+		devmem 0x200f00F4 32 0x1 && echo " | GPIO7_5 | GPIO61 | alarm_in  | Board XM 00018520                                | SoC hi3518ev200 | " | logger -t gpio_init
+		devmem 0x200f0100 32 0x1 && echo " | GPIO8_0 | GPIO64 | ircut_1   | Board JVT S130H18V and JVS/Sunwo ZB232_V200+0130 | SoC hi3518ev200 | " | logger -t gpio_init
+		devmem 0x200f0104 32 0x1 && echo " | GPIO8_1 | GPIO65 | ircut_2   | Board JVT S130H18V and JVS/Sunwo ZB232_V200+0130 | SoC hi3518ev200 | " | logger -t gpio_init
+		;;
+	esac
+}
+
 get_status_led() {
 	case $(hisilicon_board_name) in
 	hi3518ev100)
