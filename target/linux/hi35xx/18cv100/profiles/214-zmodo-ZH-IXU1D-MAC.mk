7a2b8de38fdc6902446a6823bf9d7ef0004aaee0
Sergey Sharshunov <s.sharshunov@gmail.com>
2019-10-25 22:32:06 +0500

    Create new Board hi35xx

---
+++ target/linux/hi35xx/18cv100/profiles/214-zmodo-ZH-IXU1D-MAC.mk
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2006 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/ZMODO_ZH_IXU1D_MAC
+  NAME:=Zmodo ZH-IXU1D-MAC
+  PACKAGES:=
+endef
+
+define Profile/ZMODO_ZH_IXU1D_MAC/Description
+	Zmodo IP camera, model: ZH-IXU1D-MAC, based on HI3518CV100
+endef
+$(eval $(call Profile,ZMODO_ZH_IXU1D_MAC))
