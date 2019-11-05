c397f4ad633be12331a6de1ebc795f950f26de21
Builder <builder@clipeus.crimeainfo.com>
2019-11-05 19:58:37 +0300

    Remove binary archive

--- Project_OpenIPC.sh
+++ Project_OpenIPC.sh
@@ -99,7 +99,7 @@ case $build in
   project)
     # Show project changes
     HASH1="ceddf6298ad84c0ac103d25559e4e76a57f5bf76"
-    HASH2="bb0ab9d537"
+    HASH2="6256558"
     #
     clear
     echo -e "\n####################################################################################################\n"
