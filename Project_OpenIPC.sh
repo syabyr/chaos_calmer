2b6746228ff37593182a03e8c5e34b34557b2fdf
Builder <builder@clipeus.crimeainfo.com>
2019-11-06 19:24:39 +0300

    Update script

--- Project_OpenIPC.sh
+++ Project_OpenIPC.sh
@@ -87,7 +87,7 @@ case $build in
   project)
     # Show project changes
     HASH1="ceddf6298ad84c0ac103d25559e4e76a57f5bf76"
-    HASH2="6256558"
+    HASH2="c7f3399139"
     #
     clear
     echo -e "\n####################################################################################################\n"
