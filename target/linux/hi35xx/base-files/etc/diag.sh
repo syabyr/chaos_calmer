211949a789850c1980fcb04ace464d43aae50907
Builder <builder@clipeus.crimeainfo.com>
2020-01-05 11:26:06 +0300

    Update GPIO settings

--- target/linux/hi35xx/base-files/etc/diag.sh
+++ target/linux/hi35xx/base-files/etc/diag.sh
@@ -31,23 +31,37 @@ gpio_init() {
 		devmem 0x200f0060 32 0x0   && echo " | GPIO4_3 | GPIO35 | alarm_out | Board XM 00018520                                | SoC hi3518ev200 | " | logger -t gpio_init
 		devmem 0x200f00F4 32 0x1   && echo " | GPIO7_5 | GPIO61 | alarm_in  | Board XM 00018520                                | SoC hi3518ev200 | " | logger -t gpio_init
 		#
-		devmem 0x200f00bc 32 0x0   && echo " | GPIO5_7 | GPIO47 | irled     | Board JVT S130H18V and JVS/Sunwo ZB232_V200+0130 | SoC hi3518ev200 | " | logger -t gpio_init
+		devmem 0x200f00bc 32 0x00  && echo " | GPIO5_7 | GPIO47 | light     | Board JVT S130H18V and JVS/Sunwo ZB232_V200+0130 | SoC hi3518ev200 | " | logger -t gpio_init
 		devmem 0x200f0100 32 0x1   && echo " | GPIO8_0 | GPIO64 | ircut_1   | Board JVT S130H18V and JVS/Sunwo ZB232_V200+0130 | SoC hi3518ev200 | " | logger -t gpio_init
 		devmem 0x200f0104 32 0x1   && echo " | GPIO8_1 | GPIO65 | ircut_2   | Board JVT S130H18V and JVS/Sunwo ZB232_V200+0130 | SoC hi3518ev200 | " | logger -t gpio_init
 		#
-		devmem 0x200f00F8 32 0x1   && echo " | GPIO7_6 | GPIO62 | irled     | Board Switcam HS303                              | SoC hi3518ev200 | " | logger -t gpio_init
+		devmem 0x200f0074 32 0x000 && echo " | GPIO0_1 | GPIO1  | led_red   | Board Qtech WiFi QVC-IPC-136                     | SoC hi3518ev200 | " | logger -t gpio_init
+		devmem 0x200f0078 32 0x000 && echo " | GPIO0_2 | GPIO2  | led_yel   | Board Qtech WiFi QVC-IPC-136                     | SoC hi3518ev200 | " | logger -t gpio_init
+		devmem 0x200f000c 32 0x000 && echo " | GPIO0_7 | GPIO7  | wifi_ena  | Board Qtech WiFi QVC-IPC-136                     | SoC hi3518ev200 | " | logger -t gpio_init
+		devmem 0x200f00bc 32 0x00  && echo " | GPIO5_7 | GPIO47 | ircut_1   | Board Qtech WiFi QVC-IPC-136                     | SoC hi3518ev200 | " | logger -t gpio_init
+		devmem 0x200f00c0 32 0x00  && echo " | GPIO6_0 | GPIO48 | ircut_2   | Board Qtech WiFi QVC-IPC-136                     | SoC hi3518ev200 | " | logger -t gpio_init
+		devmem 0x200f00e4 32 0x00  && echo " | GPIO7_1 | GPIO57 |           | Board                                            | SoC hi3518ev200 | " | logger -t gpio_init
+		devmem 0x200f00f0 32 0x01  && echo " | GPIO7_4 | GPIO60 |           | Board                                            | SoC hi3518ev200 | " | logger -t gpio_init
 		#
-		devmem 0x200f0030 32 0x00  && echo " | GPIO0_3 | GPIO3  | wifi_pow  | Board Switcam HS303                              | SoC hi3518ev200 | " | logger -t gpio_init
+		devmem 0x200f0074 32 0x000 && echo " | GPIO0_1 | GPIO1  | ircut_1   | Board CamHi unknown WiFi MT7601U                 | SoC hi3518ev200 | " | logger -t gpio_init
+		devmem 0x200f0078 32 0x000 && echo " | GPIO0_2 | GPIO2  | ircut_2   | Board CamHi unknown WiFi MT7601U                 | SoC hi3518ev200 | " | logger -t gpio_init
+		devmem 0x200f00c0 32 0x00  && echo " | GPIO6_0 | GPIO48 | light     | Board CamHi unknown WiFi MT7601U                 | SoC hi3518ev200 | " | logger -t gpio_init
+		#
+		devmem 0x200f00F8 32 0x1   && echo " | GPIO7_6 | GPIO62 | light     | Board Switcam HS303                              | SoC hi3518ev200 | " | logger -t gpio_init
+		#
+		devmem 0x200f0030 32 0x00  && echo " | GPIO0_3 | GPIO3  | wifi_ena  | Board Switcam HS303                              | SoC hi3518ev200 | " | logger -t gpio_init
 		#echo 3 >/sys/class/gpio/export ; echo "out" >/sys/class/gpio/gpio3/direction ; echo 1 >/sys/class/gpio/gpio3/value ; echo "Set GPIO3 wifi_pow UP"
 		#
-		devmem 0x200f00D8 32 0x000 && echo " | GPIO6_6 | GPIO54 | wifi_pow  | Board China OV9732                               | SoC hi3518ev200 | " | logger -t gpio_init
+		devmem 0x200f00D8 32 0x000 && echo " | GPIO6_6 | GPIO54 | wifi_ena  | Board China OV9732                               | SoC hi3518ev200 | " | logger -t gpio_init
 		#echo 54 >/sys/class/gpio/export ; echo "out" >/sys/class/gpio/gpio54/direction ; echo 1 >/sys/class/gpio/gpio54/value ; echo "Set GPIO54 wifi_pow UP"
+		#
+		#devmem 0x200f0004 32 0x1  && echo " | GPIO0_5 | GPIO5  |           | Reset sensor pin                                | SoC hi3518ev200 | " | logger -t gpio_init
 		;;
 
 	jvt_s130h18v)
-		devmem 0x200f00bc 32 0x0 && echo " | GPIO5_7 | GPIO47 | irled     | Board JVT S130H18V and JVS/Sunwo ZB232_V200+0130 | SoC hi3518ev200 | " | logger -t gpio_init
-		devmem 0x200f0100 32 0x1 && echo " | GPIO8_0 | GPIO64 | ircut_1   | Board JVT S130H18V and JVS/Sunwo ZB232_V200+0130 | SoC hi3518ev200 | " | logger -t gpio_init
-		devmem 0x200f0104 32 0x1 && echo " | GPIO8_1 | GPIO65 | ircut_2   | Board JVT S130H18V and JVS/Sunwo ZB232_V200+0130 | SoC hi3518ev200 | " | logger -t gpio_init
+		devmem 0x200f00bc 32 0x00  && echo " | GPIO5_7 | GPIO47 | light     | Board JVT S130H18V and JVS/Sunwo ZB232_V200+0130 | SoC hi3518ev200 | " | logger -t gpio_init
+		devmem 0x200f0100 32 0x1   && echo " | GPIO8_0 | GPIO64 | ircut_1   | Board JVT S130H18V and JVS/Sunwo ZB232_V200+0130 | SoC hi3518ev200 | " | logger -t gpio_init
+		devmem 0x200f0104 32 0x1   && echo " | GPIO8_1 | GPIO65 | ircut_2   | Board JVT S130H18V and JVS/Sunwo ZB232_V200+0130 | SoC hi3518ev200 | " | logger -t gpio_init
 		;;
 	jvt_s135h18v)
 		echo "No GPIO settings for jvt_s135h18v found" | logger
