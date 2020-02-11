83adb245ed8b68a68403166fe85d148ac34672c6
Builder <builder@clipeus.crimeainfo.com>
2020-02-11 13:19:47 +0300

    Update diag.sh

--- target/linux/hi35xx/base-files/etc/diag.sh
+++ target/linux/hi35xx/base-files/etc/diag.sh
@@ -9,18 +9,10 @@
 
 gpio_init() {
 	case $(hi35xx_board_name) in
-	hi3516cv100)
+	hi3516cv100|hi3518ev100)
 		echo "No GPIO settings for $(hi35xx_board_name) found" | logger
 		;;
-	hi3516cv200)
-		devmem 0x200f00bc 32 0x00  && echo " | GPIO5_7 | GPIO47 | light     | Board JVT S323H16VF                              | SoC hi3518ev200 | " | logger -t gpio_init
-		devmem 0x200f0100 32 0x1   && echo " | GPIO8_0 | GPIO64 | ircut_1   | Board JVT S323H16VF                              | SoC hi3516cv200 | " | logger -t gpio_init
-		devmem 0x200f0104 32 0x1   && echo " | GPIO8_1 | GPIO65 | ircut_2   | Board JVT S323H16VF                              | SoC hi3516cv200 | " | logger -t gpio_init
-		;;
-	hi3518ev100)
-		echo "No GPIO settings for $(hi35xx_board_name) found" | logger
-		;;
-	hi3518ev200)
+	hi3516cv200|hi3518ev200)
 		#
 		devmem 0x200f00C0 32 0x3   && echo " | GPIO6_0 |  AMA1  | uart1_rxd | Any Board                                        | SoC hi3518ev200 | " | logger -t gpio_init
 		devmem 0x200f00C8 32 0x3   && echo " | GPIO6_2 |  AMA1  | uart1_txd | Any Board                                        | SoC hi3518ev200 | " | logger -t gpio_init
@@ -62,44 +54,24 @@ gpio_init() {
 		#devmem 0x200f0004 32 0x1  && echo " | GPIO0_5 | GPIO5  |           | Reset sensor pin                                | SoC hi3518ev200 | " | logger -t gpio_init
 		;;
 
-	jvt_s130h18v)
-		devmem 0x200f00bc 32 0x00  && echo " | GPIO5_7 | GPIO47 | light     | Board JVT S130H18V and JVS/Sunwo ZB232_V200+0130 | SoC hi3518ev200 | " | logger -t gpio_init
-		devmem 0x200f0100 32 0x1   && echo " | GPIO8_0 | GPIO64 | ircut_1   | Board JVT S130H18V and JVS/Sunwo ZB232_V200+0130 | SoC hi3518ev200 | " | logger -t gpio_init
-		devmem 0x200f0104 32 0x1   && echo " | GPIO8_1 | GPIO65 | ircut_2   | Board JVT S130H18V and JVS/Sunwo ZB232_V200+0130 | SoC hi3518ev200 | " | logger -t gpio_init
-		;;
-	jvt_s135h18v)
-		echo "No GPIO settings for jvt_s135h18v found" | logger
-		;;
-	jvt_s323h16v)
-		echo "No GPIO settings for jvt_s323h16v found" | logger
+	jvt_s130h18v|jvt_s135h18v|jvt_s323h16v)
+		echo "No GPIO settings for jvt_* found" | logger
 		;;
 	esac
 }
 
 get_status_led() {
 	case $(hi35xx_board_name) in
-	hi3516cv100)
+	hi3516cv100|hi3518ev100)
 		#status_led="tp-link:blue:system"
 		echo "Device hi3516cv100 found - diag.sh" | logger
 		;;
-	hi3516cv200)
+	hi3516cv200|hi3518ev200)
 		echo "Device hi3516cv200 found - diag.sh" | logger
 		;;
-	hi3518ev100)
-		echo "Device hi3518ev100 found - diag.sh" | logger
-		;;
-	hi3518ev200)
-		echo "Device hi3518ev200 found - diag.sh" | logger
-		;;
 
-	jvt_s130h18v)
-		echo "Device jvt_s130h18v found - diag.sh" | logger
-		;;
-	jvt_s135h18v)
-		echo "Device jvt_s135h18v found - diag.sh" | logger
-		;;
-	jvt_s323h16v)
-		echo "Device jvt_s323h16v found - diag.sh" | logger
+	jvt_s130h18v|jvt_s135h18v|jvt_s323h16v)
+		echo "Device jvt_* found - diag.sh" | logger
 		;;
 	esac
 }
